57206d69c756c06d1e637d244f29027ff4bc6f6d
Illuminatus <theilluminatus175@gmail.com>
2018-02-06 23:13:16 +0100

    Translate list-list title

--- src/pages/list-list/list-list.module.ts
+++ src/pages/list-list/list-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ListListPage } from './list-list';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ListListPage } from './list-list';
   ],
   imports: [
     IonicPageModule.forChild(ListListPage),
+    TranslateModule.forChild()
   ],
 })
 export class ListListPageModule {}
