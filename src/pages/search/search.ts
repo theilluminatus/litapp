b96aebbd4a26dd28d76efcc522e09d097caa2e02
Illuminatus <theilluminatus175@gmail.com>
2018-02-07 01:55:50 +0100

    Add starred queries function

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { Story } from '../../models/story';
@@ -11,13 +11,16 @@ import { Stories } from '../../providers/providers';
 })
 export class SearchPage {
 
+  @ViewChild("searchbar") searchbar: any;
+  @ViewChild("panel") panel: any;
+
   currentStories: any = [];
+  starredQueries: string[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public stories: Stories) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public stories: Stories) {
+    // TODO: load starredQueries from db
+  }
 
-  /**
-   * Perform a service for the proper stories.
-   */
   getStories(ev) {
     let val = ev.target.value;
     if (!val || !val.trim()) {
@@ -30,12 +33,27 @@ export class SearchPage {
     });
   }
 
-  /**
-   * Navigate to the detail page for this item.
-   */
   openStory(story: Story) {
     this.navCtrl.push('StoryPage', {
       story: story
+    });
+  }
+
+  saveSearch(query: string) {
+    // TODO: persist starredQueries to db & check empty duplicates
+    this.starredQueries.push(query);
+  }
+
+  search(query: string) {
+    this.searchbar.value = query;
+    this.panel.close();
+  }
+
+  delete(query: string) {
+    // TODO: persist starredQueries to db
+    this.starredQueries.forEach((item, index) => {
+      if (item == query)
+        this.starredQueries.splice(index,1);
     });
   }
 
