9355d1d62141227712db32d9cf096973c99fad5d
Illuminatus <theilluminatus175@gmail.com>
2018-02-07 22:40:04 +0100

    Add list create page

--- src/pages/list-create/list-create.ts
+++ src/pages/list-create/list-create.ts
@@ -9,19 +9,16 @@ import { IonicPage, NavController, ViewController } from 'ionic-angular';
   templateUrl: 'list-create.html'
 })
 export class ListCreatePage {
-  @ViewChild('fileInput') fileInput;
 
   isReadyToSave: boolean;
-
   item: any;
-
   form: FormGroup;
 
   constructor(public navCtrl: NavController, public viewCtrl: ViewController, formBuilder: FormBuilder, public camera: Camera) {
     this.form = formBuilder.group({
-      profilePic: [''],
       name: ['', Validators.required],
-      about: ['']
+      description: [''],
+      visibility: ['public'],
     });
 
     // Watch the form for changes, and
@@ -34,48 +31,10 @@ export class ListCreatePage {
 
   }
 
-  getPicture() {
-    if (Camera['installed']()) {
-      this.camera.getPicture({
-        destinationType: this.camera.DestinationType.DATA_URL,
-        targetWidth: 96,
-        targetHeight: 96
-      }).then((data) => {
-        this.form.patchValue({ 'profilePic': 'data:image/jpg;base64,' + data });
-      }, (err) => {
-        alert('Unable to take photo');
-      })
-    } else {
-      this.fileInput.nativeElement.click();
-    }
-  }
-
-  processWebImage(event) {
-    let reader = new FileReader();
-    reader.onload = (readerEvent) => {
-
-      let imageData = (readerEvent.target as any).result;
-      this.form.patchValue({ 'profilePic': imageData });
-    };
-
-    reader.readAsDataURL(event.target.files[0]);
-  }
-
-  getProfileImageStyle() {
-    return 'url(' + this.form.controls['profilePic'].value + ')'
-  }
-
-  /**
-   * The user cancelled, so we dismiss without sending data back.
-   */
   cancel() {
     this.viewCtrl.dismiss();
   }
 
-  /**
-   * The user is done and wants to create the item, so return it
-   * back to the presenter.
-   */
   done() {
     if (!this.form.valid) { return; }
     this.viewCtrl.dismiss(this.form.value);
--- src/pages/list-list/list-list.ts
+++ src/pages/list-list/list-list.ts
@@ -23,4 +23,8 @@ export class ListListPage {
     });
   }
 
+  addList() {
+    this.navCtrl.push('ListCreatePage');
+  }
+
 }
