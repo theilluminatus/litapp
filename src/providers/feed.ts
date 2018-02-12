8d42b737b88cdfe27e98d0579fed16ced1c26c79
Illuminatus <theilluminatus175@gmail.com>
2018-02-12 18:21:46 +0100

    Persist most recent feed item

--- src/providers/feed.ts
+++ src/providers/feed.ts
@@ -5,6 +5,8 @@ import { Storage } from '@ionic/storage';
 import { FeedItem } from '../models/feeditem';
 import { Author } from '../models/author';
 import { Stories } from './stories';
+import { Authors } from './authors';
+import { FEED_KEY } from './db';
 import { Api } from './api/api';
 
 @Injectable()
@@ -13,6 +15,7 @@ export class Feed {
   constructor(
     public api: Api,
     public s: Stories,
+    public a: Authors,
     public storage: Storage
   ) { }
 
@@ -29,29 +32,24 @@ export class Feed {
     };
 
     return this.api.get('my/api/activity/wall',params,undefined,2).map((d: any) => {
-      
       if (loader) loader.dismiss();
       if (!d.data) {
         this.api.showToast();
         return [];
       }
 
-      return d.data.map(item => {
+      let items = d.data.map(item => {
         return new FeedItem({
-
           id: item.id,
           timestamp: item.when,
-          author: new Author({
-            id: item.who.userid,
-            name: item.who.username,
-            picture: item.who.userpic.currentUserpic,
-          }),
-
+          author: this.a.extractFromFeed(item.who),
           text: (!Array.isArray(item.what) ? [] : item.what),
           story: (Array.isArray(item.what) ? undefined : this.s.extractFromFeed(item))
         });
-
       });
+
+      this.storage.set(FEED_KEY, items[0].id);
+      return items;
 
     }).catch((error) => {
       if (loader) loader.dismiss();
