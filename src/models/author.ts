theilluminatus/litapp

commit e3f9e8539f5ed62367da105e3ea6ad8fc7d65bbf

Add limited refresh function

src/models/author.ts
@@ -3,17 +3,6 @@ import { Story } from './story';
 
 export class Author {
 
-  constructor(fields: any) {
-    // Quick and dirty extend/assign fields to this model
-    for (const f in fields) {
-      // @ts-ignore
-      this[f] = fields[f];
-    }
-  }
-
-}
-
-export interface Author {
   [prop: string]: any;
 
   id: any;
@@ -26,4 +15,15 @@ export interface Author {
   following: boolean;
   stories: Story[];
   favs: Story[];
+
+  constructor(fields: any) {
+    // Quick and dirty extend/assign fields to this model
+    for (const f in fields) {
+      if (typeof fields[f] !== 'function') {
+        // @ts-ignore
+        this[f] = fields[f];
+      }
+    }
+  }
+
 }

src/models/feeditem.ts
@@ -4,17 +4,6 @@ import { Story } from './story';
 
 export class FeedItem {
 
-  constructor(fields: any) {
-    // Quick and dirty extend/assign fields to this model
-    for (const f in fields) {
-      // @ts-ignore
-      this[f] = fields[f];
-    }
-  }
-
-}
-
-export interface FeedItem {
   [prop: string]: any;
 
   id: number;
@@ -22,4 +11,15 @@ export interface FeedItem {
   story: Story;
   text: string[];
   timestamp: string;
+
+  constructor(fields: any) {
+    // Quick and dirty extend/assign fields to this model
+    for (const f in fields) {
+      if (typeof fields[f] !== 'function') {
+        // @ts-ignore
+        this[f] = fields[f];
+      }
+    }
+  }
+
 }

src/models/list.ts
@@ -3,17 +3,6 @@ import { Story } from './story';
 
 export class List {
 
-  constructor(fields: any) {
-    // Quick and dirty extend/assign fields to this model
-    for (const f in fields) {
-      // @ts-ignore
-      this[f] = fields[f];
-    }
-  }
-
-}
-
-export interface List {
   [prop: string]: any;
 
   id: any;
@@ -27,4 +16,15 @@ export interface List {
   updatetimestamp: string;
 
   stories: Story[];
+
+  constructor(fields: any) {
+    // Quick and dirty extend/assign fields to this model
+    for (const f in fields) {
+      if (typeof fields[f] !== 'function') {
+        // @ts-ignore
+        this[f] = fields[f];
+      }
+    }
+  }
+
 }

src/models/story.ts
@@ -4,18 +4,6 @@ import { List } from './list';
 
 export class Story {
 
-  constructor(fields: any) {
-
-    // Quick and dirty extend/assign fields to this model
-    for (const f in fields) {
-      // @ts-ignore
-      this[f] = fields[f];
-    }
-  }
-
-}
-
-export interface Story {
   [prop: string]: any;
 
   id: any;
@@ -49,4 +37,19 @@ export interface Story {
     text: string,
     timestamp: string
   }[];
+
+  constructor(fields: any) {
+    this.updateValues(fields);
+  }
+
+  updateValues(fields: any) {
+    // Quick and dirty extend/assign fields to this model
+    for (const f in fields) {
+      if (typeof fields[f] !== 'function') {
+        // @ts-ignore
+        this[f] = fields[f];
+      }
+    }
+  }
+
 }

src/pages/story-detail/story-detail.ts
@@ -29,7 +29,7 @@ export class StoryDetailPage {
     public user: User,
     private socialSharing: SocialSharing
   ) {
-  	this.story = navParams.get('story');
+    this.story = new Story(navParams.get('story'));
 
     // load data when directly view details
     if (!this.story.cached) {
@@ -134,4 +134,12 @@ export class StoryDetailPage {
       this.stories.download(this.story);
   }
 
+  // updates only part of story
+  refreshStory() {
+    this.stories.getById(this.story.id, true).subscribe((story) => {
+      this.story.updateValues(story);
+      this.myrating = this.story.myrating;
+      this.stories.cache(this.story);
+    });
+  }
 }

src/providers/stories.ts
@@ -114,12 +114,17 @@ export class Stories {
 
 
   // Get a story by ID
-  getById(id: any) {
+  getById(id: any, force: boolean = false) {
 
-    let cached = this.stories.get(id);
-    if (cached) {
-      if (cached.length)
-        return Observable.of(cached);
+    let cached;
+    if (!force) {
+      cached = this.stories.get(id);
+      if (cached) {
+        if (cached.length)
+          return Observable.of(cached);
+      }
+    } else {
+      cached = false;
     }
 
     let filter = [{"property": "submission_id", "value": parseInt(id)}];
@@ -135,7 +140,7 @@ export class Stories {
 
       if (!cached)
         cached = new Story({
-          id: data.pages[0].submission_id,
+          id: Number.parseInt(data.pages[0].submission_id),
           title: data.pages[0].name,
           url: data.pages[0].url,
           ratingenabled: data.pages[0].allow_vote

src/pages/story-detail/story-detail.html
@@ -4,6 +4,11 @@
     <ion-title></ion-title>
     <ion-buttons end>
 
+      <button ion-button icon-only
+        (click)="refreshStory()"
+        [tooltip]="'REFRESH_BUTTON' | translate" event="press" navTooltip>
+        <ion-icon name="refresh"></ion-icon>
+      </button>
 
       <button ion-button icon-only (click)="toggleDownload()"
         [tooltip]="'STORYDETAIL_TOOLTIP_DOWNLOAD' | translate" event="press" navTooltip>

commit 72987fb8cef2c712e4ca324b7fba0991a376692d

Fix bugs introduced in e3f9e85

src/models/author.ts
@@ -3,6 +3,17 @@ import { Story } from './story';
 
 export class Author {
 
+  constructor(fields: any) {
+    // Quick and dirty extend/assign fields to this model
+    for (const f in fields) {
+      // @ts-ignore
+      this[f] = fields[f];
+    }
+  }
+
+}
+
+export interface Author {
   [prop: string]: any;
 
   id: any;
@@ -15,15 +26,4 @@ export class Author {
   following: boolean;
   stories: Story[];
   favs: Story[];
-
-  constructor(fields: any) {
-    // Quick and dirty extend/assign fields to this model
-    for (const f in fields) {
-      if (typeof fields[f] !== 'function') {
-        // @ts-ignore
-        this[f] = fields[f];
-      }
-    }
-  }
-
 }

src/models/feeditem.ts
@@ -4,6 +4,17 @@ import { Story } from './story';
 
 export class FeedItem {
 
+  constructor(fields: any) {
+    // Quick and dirty extend/assign fields to this model
+    for (const f in fields) {
+      // @ts-ignore
+      this[f] = fields[f];
+    }
+  }
+
+}
+
+export interface FeedItem {
   [prop: string]: any;
 
   id: number;
@@ -11,15 +22,4 @@ export class FeedItem {
   story: Story;
   text: string[];
   timestamp: string;
-
-  constructor(fields: any) {
-    // Quick and dirty extend/assign fields to this model
-    for (const f in fields) {
-      if (typeof fields[f] !== 'function') {
-        // @ts-ignore
-        this[f] = fields[f];
-      }
-    }
-  }
-
 }

src/models/list.ts
@@ -3,6 +3,17 @@ import { Story } from './story';
 
 export class List {
 
+  constructor(fields: any) {
+    // Quick and dirty extend/assign fields to this model
+    for (const f in fields) {
+      // @ts-ignore
+      this[f] = fields[f];
+    }
+  }
+
+}
+
+export interface List {
   [prop: string]: any;
 
   id: any;
@@ -16,15 +27,4 @@ export class List {
   updatetimestamp: string;
 
   stories: Story[];
-
-  constructor(fields: any) {
-    // Quick and dirty extend/assign fields to this model
-    for (const f in fields) {
-      if (typeof fields[f] !== 'function') {
-        // @ts-ignore
-        this[f] = fields[f];
-      }
-    }
-  }
-
 }

src/models/story.ts
@@ -4,6 +4,18 @@ import { List } from './list';
 
 export class Story {
 
+  constructor(fields: any) {
+
+    // Quick and dirty extend/assign fields to this model
+    for (const f in fields) {
+      // @ts-ignore
+      this[f] = fields[f];
+    }
+  }
+
+}
+
+export interface Story {
   [prop: string]: any;
 
   id: any;
@@ -37,19 +49,4 @@ export class Story {
     text: string,
     timestamp: string
   }[];
-
-  constructor(fields: any) {
-    this.updateValues(fields);
-  }
-
-  updateValues(fields: any) {
-    // Quick and dirty extend/assign fields to this model
-    for (const f in fields) {
-      if (typeof fields[f] !== 'function') {
-        // @ts-ignore
-        this[f] = fields[f];
-      }
-    }
-  }
-
 }

src/pages/story-detail/story-detail.ts
@@ -31,7 +31,7 @@ export class StoryDetailPage {
     private socialSharing: SocialSharing,
     private browser: BrowserTab
   ) {
-    this.story = new Story(navParams.get('story'));
+  	this.story = navParams.get('story');
 
     // load data when directly view details
     if (!this.story.cached) {
@@ -81,8 +81,6 @@ export class StoryDetailPage {
   }
 
   category(query: string) {
-
-
     this.translate.get(['STORYDETAIL_VIEWCAT','TOP', 'NEW']).subscribe(values => {
 
       let alert = this.alertCtrl.create({
@@ -139,7 +137,7 @@ export class StoryDetailPage {
   // updates only part of story
   refreshStory() {
     this.stories.getById(this.story.id, true).subscribe((story) => {
-      this.story.updateValues(story);
+      this.updateValues(story);
       this.myrating = this.story.myrating;
       this.stories.cache(this.story);
     });
@@ -147,5 +145,15 @@ export class StoryDetailPage {
 
   openLink() {
     this.browser.openUrl(this.story.url);
+  }
+
+  // quick and dirty fix
+  private updateValues(fields) {
+    for (const f in this.story) {
+      if (fields[f] !== undefined) {
+        // @ts-ignore
+        this.story[f] = fields[f];
+      }
+    }
   }
 }

src/providers/stories.ts
@@ -115,16 +115,11 @@ export class Stories {
 
   // Get a story by ID
   getById(id: any, force: boolean = false) {
-
-    let cached;
-    if (!force) {
-      cached = this.stories.get(id);
-      if (cached) {
-        if (cached.length)
-          return Observable.of(cached);
-      }
-    } else {
-      cached = false;
+    
+    let cached = this.stories.get(id);
+    if (cached && !force) {
+      if (cached.length)
+        return Observable.of(cached);
     }
 
     let filter = [{"property": "submission_id", "value": parseInt(id)}];
@@ -140,7 +135,7 @@ export class Stories {
 
       if (!cached)
         cached = new Story({
-          id: Number.parseInt(data.pages[0].submission_id),
+          id: data.pages[0].submission_id,
           title: data.pages[0].name,
           url: data.pages[0].url,
           ratingenabled: data.pages[0].allow_vote
@@ -336,7 +331,7 @@ export class Stories {
 
     let author = this.a.extractFromFeed(item.who);
     let story = new Story({
-      id: item.what.id,
+      id: item.what.id.toString(),
       title: item.what.name,
       description: item.what.description,
       category: this.g.getCategory(item.what.category_id),
@@ -366,7 +361,7 @@ export class Stories {
 
     let author = this.a.extractFromFeed(item.author);
     let story = new Story({
-      id: item.id,
+      id: item.id.toString(),
       title: item.name,
       description: item.description,
       category: this.g.getCategory(item.category_id),
@@ -396,7 +391,7 @@ export class Stories {
 
     let author = this.a.extractFromSearch(item.user);
     let story = new Story({
-      id: item.id,
+      id: item.id.toString(),
       title: item.name,
       description: item.description,
       category: item.category.name,
@@ -428,7 +423,7 @@ export class Stories {
 
     let author = this.a.extractFromNewSearch(item.author);
     let story = new Story({
-      id: item.id,
+      id: item.id.toString(),
       title: item.title,
       description: item.description,
       category: item.category_info.name,