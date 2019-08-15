88d32e2970f065909027065c4ba1b3c193751c5b
Illuminatus <theilluminatus175@gmail.com>
2019-08-15 10:59:30 +0200

    Limit amount of exported logs to 50 items

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,14 +1,15 @@
 
+declare const window: any;
+
 // keep all console logs for sending reports
-// @ts-ignore
 window.consoleLog = [];
 console = new Proxy(console, {
     get: (obj, prop) => {
         const method = obj[prop];
         return (...args) => {
             let result = method.apply(this, args);
-            // @ts-ignore
             window.consoleLog.push(JSON.parse(JSON.stringify({ prop, args })));
+            if (window.consoleLog.length > 50) window.consoleLog.shift();
             return result;
         };
     },
